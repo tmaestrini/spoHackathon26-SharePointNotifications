acd7a20aac1c94ab56b67b490db240c442853a40
Tobias Maestrini <tobias.maestrini@gmail.com>
2026-03-04 23:45:41 +0100

    feat: Refactor SPONotification and SPONotificationDialog components to use @spteck/react-controls-v2 for improved UI. - Update NotificationsCommandSet to instantiate dialog correctly. - Adjust package.json dependencies for compatibility.

--- spfx/src/extensions/components/SPONotificationDialog.tsx
+++ spfx/src/extensions/components/SPONotificationDialog.tsx
@@ -1,22 +1,41 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { BaseDialog } from '@microsoft/sp-dialog';
+import { SPFxContextAdapter } from '@spteck/react-controls-v2-spfx-adapter';
+import { ListViewCommandSetContext } from '@microsoft/sp-listview-extensibility';
+import { UniversalProvider } from '@spteck/react-controls-v2';
+import { FluentProvider, webLightTheme, webDarkTheme, IdPrefixProvider } from '@fluentui/react-components';
+import SPONotification from './SPONotification';
+import ReactDOM from 'react-dom';
 
-export default class SPONotificationDialog<T> extends BaseDialog {
-    private element: React.FunctionComponentElement<T> | null = null;
+export default class SPONotificationDialog {
+    private container: HTMLDivElement;
+    private context: ListViewCommandSetContext;
 
-    constructor(element: React.FunctionComponentElement<T>) {
-        super({ isBlocking: false });
-        this.element = element;
+    constructor(context: ListViewCommandSetContext) {
+        this.context = context;
+        this.container = document.createElement('div');
     }
 
-    public render(): void {
-        ReactDOM.render(this.element!, this.domElement);
+    public show(): Promise<void> {
+        document.body.appendChild(this.container);
+        const appContext = SPFxContextAdapter.adapt(this.context, 'SPONotificationDialog');
+        const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        
+        ReactDOM.render(
+            <IdPrefixProvider value="spo-notification-">
+                <FluentProvider theme={isDark ? webDarkTheme : webLightTheme}>
+                    <UniversalProvider context={appContext as any}>
+                        <SPONotification onClose={() => this.close()} context={this.context} />
+                    </UniversalProvider>
+                </FluentProvider>
+            </IdPrefixProvider>,
+            this.container
+        );
+        return Promise.resolve();
     }
 
-    protected onAfterClose(): void {
-        super.onAfterClose();
-        ReactDOM.unmountComponentAtNode(this.domElement);
+    public close(): void {
+        ReactDOM.unmountComponentAtNode(this.container);
+        this.container.remove();
     }
 }
 
