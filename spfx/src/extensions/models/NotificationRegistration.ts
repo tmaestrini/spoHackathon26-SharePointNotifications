217fa6632ba42abadbb5f90c67ab8af9efea0224
Tobias Maestrini <tobias.maestrini@gmail.com>
2026-03-06 23:49:28 +0100

    feat: Add NotificationRegistration type and enums for change types and notification channels

---
+++ spfx/src/extensions/models/NotificationRegistration.ts
@@ -0,0 +1,26 @@
+export type UUID = string;
+
+export type NotificationRegistration = {
+    id: UUID;
+    userId: UUID; //ENTRA ID
+    changeType: ChangeType;
+    siteId: UUID;
+    webId: UUID;
+    listId: UUID;
+    itemId?: number;
+    notificationChannel: NotificationChannel[];
+    description?: string;
+};
+
+export enum ChangeType {
+    CREATED = 'CREATED',
+    UPDATED = 'UPDATED',
+    DELETED = 'DELETED',
+    ALL = 'ALL'
+}
+
+export enum NotificationChannel {
+    Teams = 'TEAMS',
+    TeamsChannel = 'TEAMS_CHANNEL',
+    Email = 'EMAIL',
+}
